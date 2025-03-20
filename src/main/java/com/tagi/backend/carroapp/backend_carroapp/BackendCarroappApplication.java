dd3add4fcf8603059b4d06b050356ab2f352bb23
XMF3816 <ivanantonio.tronco.contractor@bbva.com>
2025-03-20 14:33:53 -0600

    init

---
+++ src/main/java/com/tagi/backend/carroapp/backend_carroapp/BackendCarroappApplication.java
@@ -0,0 +1,13 @@
+package com.tagi.backend.carroapp.backend_carroapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCarroappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCarroappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tagi/backend/carroapp/backend_carroapp/BackendCarroappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tagi.backend.carroapp.backend_carroapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCarroappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
